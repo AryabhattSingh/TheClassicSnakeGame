cfe20b84ba4c1e84a7bdf2e04e85d0911a225bd8
Aryabhatt <bhattsahab314@gmail.com>
2023-09-20 14:58:45 +0530

    Added play function in main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from scoreboard import Scoreboard
 from snake import Snake
 from wall import Wall
 
+WALL_BOUNDARY = 290
+SNAKE_SPEED = 0.1
+
 screen = Screen()
 screen.setup(width=600, height=600)
 # Full screen on startup
@@ -18,6 +21,33 @@ wall = Wall()
 snake = Snake()
 food = Food()
 score = Scoreboard()
+
+
+def play():
+    game_on = True
+    while game_on:
+        screen.update()
+        time.sleep(SNAKE_SPEED)
+        snake.move()
+
+        # Detect collision with wall
+        if abs(snake.HEAD.xcor()) > WALL_BOUNDARY or abs(snake.HEAD.ycor()) > WALL_BOUNDARY:
+            score.write_game_over()
+            game_on = False
+
+        # Detect collision with food
+        if snake.HEAD.distance(food) < 15:
+            snake.increase_length()
+            food.refresh()
+            score.increase_score()
+
+        # Detect collision with body
+        # If the snake head collides with any segment then trigger game over
+        for segment in snake.snake_segments[1:]:
+            if snake.HEAD.distance(segment) < 10:
+                score.write_game_over()
+                game_on = False
+
 
 screen.update()
 
