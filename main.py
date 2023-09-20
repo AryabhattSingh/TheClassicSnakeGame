f215b6abac57a51e724b6fb39357145c0415c2ef
Aryabhatt <bhattsahab314@gmail.com>
2023-09-20 13:47:18 +0530

    Used wall class in main.py to set up the wall on the game screen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
 from turtle import Screen
 
+from wall import Wall
+
 screen = Screen()
 screen.setup(width=600, height=600)
 # Full screen on startup
@@ -9,5 +11,10 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
+wall = Wall()
+
+screen.update()
+
+screen.listen()
 
 screen.exitonclick()
