285f4174b6ed150b6796f0470f109d11525adebc
Aryabhatt <bhattsahab314@gmail.com>
2023-09-30 13:18:37 +0530

    Added font, alignment, chamged constructor to read highscore from data.txt, changed write_score() to include font, modified increase_score to update data.txt as soon as score crosses previous high score, modified write_game_ove() to include font

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,8 @@
 from turtle import Turtle
 
-GAME_OVER_X_POS = -20
+FONT = ("Courier", 24, "normal")
+ALIGNMENT = "center"
+GAME_OVER_X_POS = -60
 GAME_OVER_Y_POS = 0
 
 
@@ -8,6 +10,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as file:
+            contents = file.read()
+        self.high_score = int(contents)
         self.write_score()
 
     def write_score(self):
@@ -15,11 +20,15 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.penup()
         self.color("violet")
-        self.goto(-10, 290)
-        self.write(arg=f"Score : {self.score}", align="center")
+        self.goto(0, 290)
+        self.write(arg=f"Score : {self.score}   High Score : {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
         self.write_score()
 
     def write_game_over(self):
@@ -27,4 +36,4 @@ class Scoreboard(Turtle):
         self.color("white")
         self.penup()
         self.goto(GAME_OVER_X_POS, GAME_OVER_Y_POS)
-        self.write("Game Over")
+        self.write("Game Over", font=FONT)
